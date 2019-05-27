367bf763c1d04a923a1eb48c5d409467a419185a
Uladzislau Kavaliou <32036669+kkawalski@users.noreply.github.com>
2019-05-27 11:07:19 +0300

    Add comments

--- bot/money.py
+++ bot/money.py
@@ -1,14 +1,14 @@
 import requests
 
 def get_money(needed):
-    url = "http://www.nbrb.by/API/ExRates/Rates?Periodicity=0"
+    url = "http://www.nbrb.by/API/ExRates/Rates?Periodicity=0" #json с курсами валют за сегодня
     courses = requests.get(url).json()
     for course in courses:
-    	if needed.upper() in course['Cur_Abbreviation']:
-    		return "За {} курс {} BYN за {} {}".format(
+    	if needed.upper() in course['Cur_Abbreviation']: # ищем запрашиваемую валюту
+    		return "За {} курс {} BYN за {} {}".format( 
                                     course['Date'][:10],
     				                course['Cur_OfficialRate'],
     				                course['Cur_Scale'],
     				                course['Cur_Abbreviation']
-                                    )
-    return 'Unknown money abbreviation'
+                                    ) #если нашли, возвращаем строку в таком формате
+    return 'Unknown money abbreviation' #если не нашли, возврщаем сообщение, что валют нет
