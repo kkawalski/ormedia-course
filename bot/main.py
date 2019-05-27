7de97e01aef59f0f38a88eb536299123defcc97d
Uladzislau Kavaliou <32036669+kkawalski@users.noreply.github.com>
2019-05-27 11:04:38 +0300

    Add comments

--- bot/main.py
+++ bot/main.py
@@ -10,30 +10,30 @@ def main(money_bot):
     headers = {
             'accept': '*/*',
             'user-agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
-            }
+            } #для имитации работы браузера
     vacancy_url = 'https://jobs.tut.by/search/vacancy?area=1002&text=python&page=0'
-    jobs = vacancy.tut_parse(vacancy_url, headers)
+    jobs = vacancy.tut_parse(vacancy_url, headers) #список вакансий на первой странице тут бая
 
     while True:
-        money_bot.get_updates(offset=new_offset)
+        money_bot.get_updates(offset=new_offset) #получили начальные обновления
 
-        last_update = money_bot.get_last_update()
+        last_update = money_bot.get_last_update() #получили последнее
 
-        answer = money_bot.get_message()
-        chat_id = answer['chat_id']
-        text = answer['text']
+        answer = money_bot.get_message() #считали сообщение для бота
+        chat_id = answer['chat_id'] #чат айди пользователя
+        text = answer['text'] #текст, отправеленный пользователем
 
-        if text[0] == '/':
-            if text[1:] == 'vacancy':
-                job = jobs[randint(0, len(jobs)-1)]
+        if text[0] == '/': #команды начинаются с /
+            if text[1:] == 'vacancy': #запрос вакансий
+                job = jobs[randint(0, len(jobs)-1)] #выбираем случайную вакансию с первой страницы
                 mess = "{}\n{}\n{}\n{}".format(job['title'], job['href'], job['company'], job['content'])
-                money_bot.send_message(chat_id, mess)
+                money_bot.send_message(chat_id, mess) #отправляем сообщение пользователю, который запросил(по chat_id)
             else:
-                money_bot.send_message(chat_id, get_money(text[1:]))
+                money_bot.send_message(chat_id, get_money(text[1:])) #в противнов случае считаем команду аббревиатурой валюты и отправляем ее курс
         else:
             money_bot.send_message(chat_id, 'Command starts with "/"')
 
-        new_offset = last_update['update_id'] + 1
+        new_offset = last_update['update_id'] + 1 #смещаем идентификатор первого возвращаемого обновления(должен быть на 1 больше чем последнее(так написано в апи телеги))
 
 
 if __name__ == '__main__':
