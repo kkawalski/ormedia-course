22b1ea3d40f484b744ad01c5afd791aec59c3ff3
Uladzislau Kavaliou <32036669+kkawalski@users.noreply.github.com>
2019-05-27 11:27:32 +0300

    Add comments

--- bot/base.py
+++ bot/base.py
@@ -4,21 +4,21 @@ class BaseBot:
 
     def __init__(self, token):
         self.token = token
-        self.url = 'https://api.telegram.org/bot{}/'.format(token)
+        self.url = 'https://api.telegram.org/bot{}/'.format(token) 
 
     def get_updates(self, offset=None, timeout=30):
-        method = 'getUpdates'
-        params = {'timeout': timeout, 'offset': offset}
-        resp = requests.get(self.url + method, params)
-        return resp.json()['result']
+        method = 'getUpdates' #метод для апи
+        params = {'timeout': timeout, 'offset': offset} #тамй-аут для лонг поллинга(сервер может не сразу оветить), оффсет - идентификатор первого возвращаемого обновления
+        resp = requests.get(self.url + method, params) #запрашиваем обновления
+        return resp.json()['result'] #возврщаем поле результата из словаря
 
     def send_message(self, chat_id, text):
-        method = 'sendMessage'
-        params = {'chat_id': chat_id, 'text': text}
-        resp = requests.post(self.url + method, params)
+        method = 'sendMessage' #метод для апи
+        params = {'chat_id': chat_id, 'text': text} #айди чата с пользователем, текст отправленный пользователю
+        resp = requests.post(self.url + method, params) #отправление сообщения пользователю
         return resp
 
-    def get_message(self):
+    def get_message(self): #достаем сообщение из последнего обновления
         data = self.get_last_update()
         chat_id = data['message']['chat']['id']
         message_text = data['message']['text']
@@ -28,7 +28,7 @@ class BaseBot:
                    }
         return message
 
-    def get_last_update(self):
+    def get_last_update(self): #берем последнее обновление
         get_result = self.get_updates()
 
         if len(get_result) > 0:
